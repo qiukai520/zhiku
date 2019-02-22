6129d3fd70d0f58663bcf94d328287f3db14ea5f
lizhihong <2419636244@qq.com>
2019-02-22 14:26:02 +0800

    修复首页手机端自适应问题

--- practice.py
+++ practice.py
@@ -83,3 +83,13 @@ class UserViewSet(ModelViewSet):
 
 
 
+##########test############
+
+
+class Singleton(object):
+    _instance = None
+
+    def __new__(cls,*args,**kwargs):
+        if not cls._instance:
+            cls._instance = super(Singleton,cls).__new__(cls,*args,**kwargs)
+        return cls._instance
